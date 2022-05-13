3bbfcd1af6b341c3f4550fbc540c3ac00e62355e
Imranbek Aliev <imranbek.aliev@zoodmall.com>
2022-05-13 16:29:52 +0300

    main

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+
+	"mock-service/internal/handler"
+)
+
 func main() {
 
+	h := handler.NewHandler()
+	r := chi.NewRouter()
+
+	r.Get("/hello", h.Hello)
+
+	err := http.ListenAndServe(":8080", r)
+	log.Fatal(err)
 }
